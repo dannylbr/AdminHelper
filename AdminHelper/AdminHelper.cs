12bdf78dd4217330dfb101de41813ac43794881d
Daniel Soares <dannylbr@outlook.com>
2025-10-27 11:13:12 -0300

    Adicionar arquivos de projeto.

---
+++ AdminHelper/AdminHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+
+namespace AdminHelper;
+
+
+public static class AdminHelper
+{
+    public static bool IsAdministrator()
+    {
+        using WindowsIdentity identity = WindowsIdentity.GetCurrent();
+        WindowsPrincipal principal = new(identity);
+        return principal.IsInRole(WindowsBuiltInRole.Administrator);
+    }
+
+    public static void RestartAsAdministrator()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo(Application.ExecutablePath) { Verb = "runas" });
+            Application.Exit();
+        }
+        catch
+        {
+            MessageBox.Show("Administrator permission required to remove entries.", "Permission Denied",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
